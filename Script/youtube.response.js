305cc5760fd89fd93f060ae11f7f8814b0c80800
tranhuyen-tech <tranhuyen.intops@gmail.com>
2026-03-16 19:30:57 +0700

    Update youtube.response.js

--- Script/youtube.response.js
+++ Script/youtube.response.js
@@ -3,20 +3,19 @@ let body = $response.body;
 try {
 let obj = JSON.parse(body);
 
-// enable background
-obj.playabilityStatus && (obj.playabilityStatus.backgroundPlayback = {allowed:true});
+if (obj.playabilityStatus) {
+obj.playabilityStatus.backgroundPlayback = {allowed: true};
+}
 
-// enable pip
-obj.playerConfig && (obj.playerConfig.audioConfig = {backgroundable:true});
+if (obj.playerConfig) {
+obj.playerConfig.audioConfig = {backgroundable: true};
+}
 
-// remove ads
 delete obj.adPlacements;
 delete obj.playerAds;
 delete obj.adSlots;
-delete obj.adBreakHeartbeatParams;
-delete obj.adBreakParams;
 
-$done({body:JSON.stringify(obj)});
+$done({body: JSON.stringify(obj)});
 
 } catch(e) {
 $done({body});
